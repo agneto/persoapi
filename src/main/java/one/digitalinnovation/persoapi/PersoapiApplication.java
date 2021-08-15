7275bd6db3935e45b54f0bc0a20fd817f0569238
Abilio Neto <sis.neto@gmail.com>
2021-08-15 14:45:52 -0300

    commit inicial da person api

---
+++ src/main/java/one/digitalinnovation/persoapi/PersoapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.persoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/persoapi/PersoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.persoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
