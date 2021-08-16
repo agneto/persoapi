a6efd02a883d78ba0e2efa052a954e3f7bb2d856
Abilio Neto <sis.neto@gmail.com>
2021-08-15 22:59:07 -0300

    Developed delete by Id operation.

--- src/main/java/one/digitalinnovation/persoapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/persoapi/controller/PersonController.java
@@ -34,4 +34,10 @@ public class PersonController {
         return this.personService.findById(id);
     }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        this.personService.delete(id);
+    }
+
 }
